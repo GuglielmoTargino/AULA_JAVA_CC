c17955d8a5c0651c6582569326cb692c04d31dca
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-29 01:32:06 -0300

    teste_com_bd
    Exercício conexão com BD.

---
+++ TESTE_CONN_BD/src/TesteComBD.java
@@ -0,0 +1,39 @@
+
+//import Java.sql.Connection; 
+//import Java.sql.Statement;
+
+import java.sql.Connection;// serve para conectar com o BD
+import java.sql.Statement;// serve para conectar com o BD
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;// serve para conectar com o BD
+import java.sql.SQLException;// serve para conectar com o BD
+
+
+public class TesteComBD {
+
+    
+    public static void main(String[] args) {
+        try {
+            // TODO code application logic here
+            Connection conn; //variavel da classe connection para conexão
+            Statement st;// serve para permitir execuat escrita no BD
+            Class.forName("com.mysql.cj.jdbc.Driver"); //classe do drive que faz conexaõ com o BD.
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cadastro","root",""); 
+            st=conn.createStatement();
+            st.executeUpdate("INSERT INTO funcionario (cargo,email,nome,salario,tel) VALUES('gato', 'toyy@te@','joe',1000,'5289-2')");
+            JOptionPane.showMessageDialog(null,"Processo concluido");
+        } catch (ClassNotFoundException ex) { // caso não encontre a biblioteca, mosta "Erro de biblioteca"
+            JOptionPane.showMessageDialog(null,"Erro de biblioteca");
+            
+        } catch (SQLException ex) { // caso não execute o script sql mostra "falha de processo "
+            JOptionPane.showMessageDialog(null,"falha de processo ");
+        }
+        
+        
+        
+        
+    }
+
+}
+    
+
