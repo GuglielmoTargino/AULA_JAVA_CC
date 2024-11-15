25710fb95d3f553fea692fdb08b42a9223cbfb7d
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-11-15 17:35:44 -0300

    teste
    Testes apenas

--- TESTE_CONN_BD/src/TesteComBD.java
+++ TESTE_CONN_BD/src/TesteComBD.java
@@ -20,12 +20,24 @@ public class TesteComBD {
         try {
             // TODO code application logic here
             Connection conn; //variavel da classe connection para conexão
-            Statement st;// serve para permitir execuat escrita no BD
+            Statement st;// serve para permitir executar escrita no BD
+            
+            //MSSql >> no oracle não precisa indicar o nome do BD
+            //Pois quando faz login de usuario cria uma conta automaticamente.
+            String url="jdbc:sqlserver://localhost:1433;databaseName=connec_test";
+            String username = "ght";
+            String password = "4004";
+            
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");//caminho da classe do drive que faz conexão com o BD.
+            conn = DriverManager.getConnection( url,username, password);  
+            
+            //String url = "jdbc:sqlserver://localhost:1433;databaseName=meuBanco";
+            
             
             //Oracle >> no oracle não precisa indicar o nome do BD
             //Pois quando faz login de usuario cria uma conta automaticamente.
-            Class.forName("oracle.jdbc.driver.OracleDriver");//caminho da classe do drive que faz conexão com o BD.
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","ght","4004");
+            //Class.forName("oracle.jdbc.driver.OracleDriver");//caminho da classe do drive que faz conexão com o BD.
+            //conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","ght","4004");
             
             //MySql
             //Class.forName("com.mysql.cj.jdbc.Driver"); //caminho da classe do drive que faz conexão com o BD.
@@ -34,7 +46,8 @@ public class TesteComBD {
            
             
             st=conn.createStatement(); //autentica a conexão feita com BD.
-            st.executeUpdate("insert into pets(nome) values('espos')");              
+            st.executeUpdate("insert into pets(id,nome) values(15,'mama')"); 
+             //st.executeUpdate("insert into pets(nome) values('marid')");
             JOptionPane.showMessageDialog(null,"Cadastro bem Sucedido");
             
         } catch (ClassNotFoundException ex) { // caso não encontre a biblioteca, mosta "Erro de biblioteca"
