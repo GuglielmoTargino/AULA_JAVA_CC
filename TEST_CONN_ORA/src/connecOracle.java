e9e458826d34bd98508633d7e98de7ce998869c5
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-12-13 12:14:10 -0300

    ajuste
    Ajuste apenas

--- TEST_CONN_ORA/src/connecOracle.java
+++ TEST_CONN_ORA/src/connecOracle.java
@@ -31,15 +31,15 @@ public class connecOracle {
             
              
              st=conn.createStatement(); //autentica a conexão feita com BD.
-             //st.executeUpdate("insert into pets(nome) values('ARMOR')");
-             st.executeUpdate("delete from pets where nome='jaha'");
+             st.executeUpdate("insert into pets(nome) values('gug')");
+             //st.executeUpdate("delete from pets where nome='jaha'");
             JOptionPane.showMessageDialog(null,"Processo bem Sucedido");
             
         } catch (ClassNotFoundException ex) { // caso não encontre a biblioteca, mosta "Erro de biblioteca"
-            JOptionPane.showMessageDialog(null,"Erro de Biblioteca");
+            JOptionPane.showMessageDialog(null,"Erro de Biblioteca"+ex.getMessage());
             
         } catch (SQLException ex) { // caso não execute o script sql mostra "falha de processo "
-            JOptionPane.showMessageDialog(null,"Falha de dado recusados ou incorretos");
+            JOptionPane.showMessageDialog(null,"Falha de dado recusados ou incorretos"+ex.getMessage());
             
         } 
         
