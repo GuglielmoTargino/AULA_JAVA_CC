b06e6332b4f758c56b28b2f90ccf46f6097eb0f0
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-12-18 11:09:59 -0300

    revisao
    Revisao exercício aula sistema dao

--- CRUD_SOFISTICADO/src/dados_conexao_bd/SistemaDao.java
+++ CRUD_SOFISTICADO/src/dados_conexao_bd/SistemaDao.java
@@ -14,7 +14,7 @@ public class SistemaDao {
 
     public static Connection conectar_() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver"); //classe do drive que faz conexaõ com o BD. 
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cadastro", "root", ""); //variavel da classe connection para conexão
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "ght", "4004"); //variavel da classe connection para conexão
         return conn;
     }
 
@@ -23,7 +23,7 @@ public class SistemaDao {
     //                          =============================
     public static ResultSet fazerLogin_(String u, String s) throws ClassNotFoundException, SQLException {
         Connection conn = SistemaDao.conectar_();
-        PreparedStatement st = conn.prepareStatement("SELECT * FROM usuario WHERE usuario = ?  AND senha = ? ");  // comando query no BD
+        PreparedStatement st = conn.prepareStatement("SELECT * FROM usuario WHERE nome_usu= ?  AND senha = ? ");  // comando query no BD
         st.setString(1, u);
         st.setString(2, s);
         ResultSet resultado = st.executeQuery(); //aqui resultado guarda o valor encontrado no BD.
